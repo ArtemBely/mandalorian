93deeaaf0f7e665ae9eb2418ca6d61ef6822770c
Артем Белышев <you@example.com>
2022-06-11 18:30:01 +0200

    mail2

--- scripts/script1.js
+++ scripts/script1.js
@@ -2,16 +2,16 @@ $(document).ready(function() {
 
 	//E-mail Ajax Send
 	$("#our_form").submit(function() { //Change
-    swal({
-    text: "Message was sent. Our team will contact you soon.",
-    button: "OK"
-    });
 		var th = $(this);
 		$.ajax({
 			type: "POST",
 			url: "mail.php", //Change
 			data: th.serialize()
 		}).done(function() {
+      swal({
+      text: "Message was sent. Our team will contact you soon.",
+      button: "OK"
+      });
       console.log('done');
 			setTimeout(function() {
 				// Done Functions
